eb10e292310b40e38619cae1fbdfa8a6fd864e30
Andrey <andryukha.chipizybov@mail.ru>
2021-01-07 11:29:12 +0300

    доделал верстку

--- src/App.js
+++ src/App.js
@@ -5,10 +5,38 @@ function App() {
     <div className="App">
       <h1>Редактор текста...</h1>
       <div className="list">
-        <div className="listHeader"><p>Редактор текста...</p></div>
+        <div className="listHeader">
+          <input type="button" value="Ж"></input>
+          <input type="button" value="К"></input>
+          <input type="button" value="Ч"></input>
+          <input type="button" value="ВР"></input>
+          <input type="button" value="НР"></input>
+          <select>
+            <option>10</option>
+            <option>11</option>
+            <option>12</option>
+            <option>14</option>
+            <option>16</option>
+            <option>18</option>
+            <option>20</option>
+            <option>22</option>
+            <option>24</option>
+          </select>
+          <input type="button" value="Фон"></input>
+          <input type="button" value="Вставить"></input>
+          <input type="button" value="&equiv;"></input>
+          <input type="button" value="Очистить"></input>
+          <input type="button" className="del" value="Cохранить"></input>
+        </div>
         <div className="listBody">
-          <div className="listMeny"><p>Редактор текста...</p></div>
-          <div className="listSpase"><p>Редактор текста...</p></div>
+          <div className="listMeny">
+            <input type="button" value="1"></input>
+            <input type="button" value="2"></input>
+            <input type="button" value="3"></input>
+          </div>
+          <div className="listSpase">
+            <textarea/>
+          </div>
         </div>
       </div>
     </div>
